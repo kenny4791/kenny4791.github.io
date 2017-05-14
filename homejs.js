93938f3c5ed0a2e269c86f8c736b0b09362bd646
kenny4791 <guan4791@gmail.com>
2017-05-14 18:03:58 +0800

    update1

---
+++ homejs.js
@@ -0,0 +1,27 @@
+function widthset(){
+	document.getElementById('titlebackground').style.width = '100%';
+	document.getElementById('titlebackground').style.height = '80%';
+}
+
+function menuhide(){
+	var menudisplay = document.getElementById('menu').style.display;
+	if(menudisplay==="block"){
+		menudisplay = "none";
+	}
+	else if(menudisply==="none"){
+		menudispaly = "block";
+	}
+
+}
+
+function showchap1(){
+	var ch1 = document.getElementById('chap1').style.display;
+	var ch2 = document.getElementById('chap2').style.display;
+	var ch3 = document.getElementById('chap3').style.display;
+	var ch4 = document.getElementById('chap4').style.display;
+	var ch5 = document.getElementById('chap5').style.display;
+
+	if(){
+
+	}
+}
