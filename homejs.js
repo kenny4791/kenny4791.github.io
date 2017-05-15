bf8341b8d9465a29a223ec10bae4266358879fc5
kenny4791 <guan4791@gmail.com>
2017-05-15 12:24:21 +0800

    update6
    menu problem solved

--- homejs.js
+++ homejs.js
@@ -9,47 +9,48 @@ function menuhide(){
 
 function unlock1(){
 	document.getElementById("image1").src="unlocked.jpg";
-	document.getElementById("chap1").display="block";
-	document.getElementById("chap2").display="none";
-	document.getElementById("chap3").display="none";
-	document.getElementById("chap4").display="none";
-	document.getElementById("chap5").display="none";
+	document.getElementById("chap1").style.display="block";
+	document.getElementById("chap2").style.display="none";
+	document.getElementById("chap3").style.display="none";
+	document.getElementById("chap4").style.display="none";
+	document.getElementById("chap5").style.display="none";
 }
 
 function unlock2(){
 	document.getElementById("image2").src="unlocked.jpg";
-	document.getElementById("chap1").display="none";
-	document.getElementById("chap2").display="block";
-	document.getElementById("chap3").display="none";
-	document.getElementById("chap4").display="none";
-	document.getElementById("chap5").display="none";
+	document.getElementById("chap1").style.display="none";
+	document.getElementById("chap2").style.display="block";
+	document.getElementById("chap3").style.display="none";
+	document.getElementById("chap4").style.display="none";
+	document.getElementById("chap5").style.display="none";
 }
+
 
 function unlock3(){
 	document.getElementById("image3").src="unlocked.jpg";
-	document.getElementById("chap1").display="none";
-	document.getElementById("chap2").display="none";
-	document.getElementById("chap3").display="block";
-	document.getElementById("chap4").display="none";
-	document.getElementById("chap5").display="none";
+	document.getElementById("chap1").style.display="none";
+	document.getElementById("chap2").style.display="none";
+	document.getElementById("chap3").style.display="block";
+	document.getElementById("chap4").style.display="none";
+	document.getElementById("chap5").style.display="none";
 }
 
 function unlock4(){
 	document.getElementById("image4").src="unlocked.jpg";
-	document.getElementById("chap1").display="none";
-	document.getElementById("chap2").display="none";
-	document.getElementById("chap3").display="none";
-	document.getElementById("chap4").display="block";
-	document.getElementById("chap5").display="none";
+	document.getElementById("chap1").style.display="none";
+	document.getElementById("chap2").style.display="none";
+	document.getElementById("chap3").style.display="none";
+	document.getElementById("chap4").style.display="block";
+	document.getElementById("chap5").style.display="none";
 }
 
 function unlock5(){
 	document.getElementById("image5").src="unlocked.jpg";
-	document.getElementById("chap1").display="none";
-	document.getElementById("chap2").display="none";
-	document.getElementById("chap3").display="none";
-	document.getElementById("chap4").display="none";
-	document.getElementById("chap5").display="block";
+	document.getElementById("chap1").style.display="none";
+	document.getElementById("chap2").style.display="none";
+	document.getElementById("chap3").style.display="none";
+	document.getElementById("chap4").style.display="none";
+	document.getElementById("chap5").style.display="block";
 }
 
 window.onscroll = function() {scrollFunction()};
