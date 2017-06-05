68f7a162020b54ae964c0a9f814d519b189f4661
kenny4791 <guan4791@gmail.com>
2017-06-05 17:15:36 +0800

    update9
    new template! XD

---
+++ novelJS.js
@@ -0,0 +1,60 @@
+var charA,charB,charC,charD,charE;
+charA=0;
+charB=0;
+charC=0;
+charD=0;
+charE=0;
+var min1,min2,min3;
+
+function opta(){
+	document.getElementById("a").style.display="block";
+	document.getElementById("b").style.display="none";
+	document.getElementById("c").style.display="none";
+	document.getElementById("d").style.display="none";
+	document.getElementById("e").style.display="none";
+	charA=1;
+}
+
+function optb(){
+	document.getElementById("a").style.display="block";
+	document.getElementById("b").style.display="none";
+	document.getElementById("c").style.display="none";
+	document.getElementById("d").style.display="none";
+	document.getElementById("e").style.display="none";
+	charA=1;
+}
+
+function optc(){
+	document.getElementById("a").style.display="block";
+	document.getElementById("b").style.display="none";
+	document.getElementById("c").style.display="none";
+	document.getElementById("d").style.display="none";
+	document.getElementById("e").style.display="none";
+	charA=1;
+}
+
+function optd(){
+	document.getElementById("a").style.display="none";
+	document.getElementById("b").style.display="none";
+	document.getElementById("c").style.display="none";
+	document.getElementById("d").style.display="none";
+	document.getElementById("e").style.display="none";
+	charA=1;
+}
+
+function opte(){
+	document.getElementById("a").style.display="none";
+	document.getElementById("b").style.display="none";
+	document.getElementById("c").style.display="none";
+	document.getElementById("d").style.display="none";
+	document.getElementById("e").style.display="none";
+	charA=1;
+}
+
+function msnClose(){
+	document.getElementById("msn").style.display="none";
+}
+
+function msnOpen(){
+	document.getElementById("msn").style.display="block";
+}
