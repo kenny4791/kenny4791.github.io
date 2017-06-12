95abb4ac504f6fc28feb0cf8d81273fa68da80da
kenny4791 <guan4791@gmail.com>
2017-06-12 13:09:02 +0800

    new

--- novelJS.js
+++ novelJS.js
@@ -1,60 +1,25 @@
-var charA,charB,charC,charD,charE;
-charA=0;
-charB=0;
-charC=0;
-charD=0;
-charE=0;
-var min1,min2,min3;
-
-function opta(){
-	document.getElementById("a").style.display="block";
-	document.getElementById("b").style.display="none";
-	document.getElementById("c").style.display="none";
-	document.getElementById("d").style.display="none";
-	document.getElementById("e").style.display="none";
-	charA=1;
+function msnClosea(){
+	document.getElementById("msna").style.display="none";
 }
 
-function optb(){
-	document.getElementById("a").style.display="block";
-	document.getElementById("b").style.display="none";
-	document.getElementById("c").style.display="none";
-	document.getElementById("d").style.display="none";
-	document.getElementById("e").style.display="none";
-	charA=1;
+function msnOpena(){
+	document.getElementById("msna").style.display="block";
 }
 
-function optc(){
-	document.getElementById("a").style.display="block";
-	document.getElementById("b").style.display="none";
-	document.getElementById("c").style.display="none";
-	document.getElementById("d").style.display="none";
-	document.getElementById("e").style.display="none";
-	charA=1;
+
+function msnCloseb(){
+	document.getElementById("msnb").style.display="none";
 }
 
-function optd(){
-	document.getElementById("a").style.display="none";
-	document.getElementById("b").style.display="none";
-	document.getElementById("c").style.display="none";
-	document.getElementById("d").style.display="none";
-	document.getElementById("e").style.display="none";
-	charA=1;
+function msnOpenb(){
+	document.getElementById("msnb").style.display="block";
 }
 
-function opte(){
-	document.getElementById("a").style.display="none";
-	document.getElementById("b").style.display="none";
-	document.getElementById("c").style.display="none";
-	document.getElementById("d").style.display="none";
-	document.getElementById("e").style.display="none";
-	charA=1;
+
+function msnClosec(){
+	document.getElementById("msnc").style.display="none";
 }
 
-function msnClose(){
-	document.getElementById("msn").style.display="none";
-}
-
-function msnOpen(){
-	document.getElementById("msn").style.display="block";
+function msnOpenc(){
+	document.getElementById("msnc").style.display="block";
 }
